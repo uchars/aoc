154d55419f2ca854cab797065f1e09c51b674139
Nils <40796807+uchars@users.noreply.github.com>
2023-12-02 11:24:53 +0100

    better late than never i guess

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -4,6 +4,26 @@ use std::io::{self, BufRead};
 const ASCII_0: u8 = 0x30;
 const ASCII_9: u8 = 0x39;
 
+const DIGITS: [&str; 18] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9",
+];
+
+fn str_to_num(digit: &str) -> u8 {
+    match digit {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        n => n.parse::<u8>().unwrap(),
+    }
+}
+
 fn get_num(text: String) -> Option<u64> {
     let mut num: u8 = 0;
     let mut found_first = false;
@@ -28,6 +48,38 @@ fn get_num(text: String) -> Option<u64> {
     }
 }
 
+pub fn solve2() {
+    let mut result: u64 = 0;
+    let file = File::open("input2").unwrap();
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let mut f_pos = Option::None;
+        let mut f_digit = Option::None;
+        let mut l_pos = Option::None;
+        let mut l_digit = Option::None;
+        for digit in DIGITS {
+            if let Some(pos) = line.as_ref().unwrap().find(digit) {
+                if pos <= f_pos.unwrap_or(pos) {
+                    f_pos = Some(pos);
+                    f_digit = Some(digit);
+                }
+            }
+            if let Some(pos) = line.as_ref().unwrap().rfind(digit) {
+                if pos >= l_pos.unwrap_or(pos) {
+                    l_pos = Some(pos);
+                    l_digit = Some(digit);
+                }
+            }
+        }
+        let first = f_digit.map(str_to_num).unwrap();
+        let last = l_digit.map(str_to_num).unwrap();
+        result += (first as u64) * 10 + (last as u64);
+    }
+
+    println!("Solution 2: {}", result);
+}
+
 fn solve() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("input")?;
     let reader = io::BufReader::new(file);
@@ -47,4 +99,5 @@ fn main() {
         Ok(_) => println!("solve success"),
         Err(e) => println!("could not open file: {}", e.to_string()),
     };
+    solve2();
 }
