d53bdfd92f52a212cc48e1ac03df547f59b726a7
Nils <40796807+uchars@users.noreply.github.com>
2023-12-02 11:33:59 +0100

    stable pkgs

--- 2023/day01/flake.nix
+++ 2023/day01/flake.nix
@@ -2,7 +2,7 @@
   description = "Rust devshell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
